f9141724ea3c99eb607a0ee7cc52a22f64f8faa2
Abadi Jerry <abadijerry44@gmail.com>
2025-02-15 13:11:34 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,64 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app: Express = express();
+
+const DB = process.env.CONNECTION_STRING || '';
+
+app.use(express.json());
+app.use(cors());
+
+//security http headers
+app.use(helmet());
+
+//rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100, //max of 100 requests
+  message:
+    'Too many requests from this IP address, please try again in 15 minutes',
+});
+app.use('/api', limiter);
+
+//data sanitization against noSql query injection
+app.use(mongoSanitize());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.requestTime = new Date().toISOString();
+  console.log('Request Time:', req.requestTime);
+  next();
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'welocome to your shopping community',
+  });
+});
+
+app.get('*', (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    message: `no route found ${req.originalUrl}`,
+  });
+  // next();
+});
+
+
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch((err: any) => {
+    console.error('DB connection error:', err);
+    process.exit(1);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
